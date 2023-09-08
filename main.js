5f086ac322b599b40729be5c56082bac922cb81e
Otieno <ronvi035@gmail.com>
2023-09-08 11:10:25 +0300

    self practice code

--- main.js
+++ main.js
@@ -1,37 +1,81 @@
-let playGame = confirm('shall we play rock, paper or scissors');
+// let playGame = confirm('shall we play rock, paper or scissors');
+
+// if (playGame){
+//     //play
+//     let playerChoice = prompt('please enter rock, paper or scissor.');
+
+//     if (playerChoice){
+//         //continue to play
+//         let playerOne = playerChoice.trim().toLowerCase();
+//         if(playerOne === 'rock' || playerOne === 'paper' || playerOne === 'scissor'){
+
+//             let computerChoice = Math.floor(Math.random() *3 +1);
+//             let computer = computerChoice === 1 ? 'rock' : computerChoice === 2 ? 'paper' : 'scissor';
+
+//             let result = 
+//             playerOne === computer ? `Tie game: \n${playerOne}\n${computer}`
+//             : playerOne === 'rock' && computer === 'paper' ?
+//             `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
+//             : playerOne === 'paper' && computer === 'rock' ?
+//             `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
+//             : playerOne === 'scissor' && computer === 'rock' ?
+//             `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
+//             : `playerOne: ${playerOne}\ncomputer: ${computer}\nplayerOne wins!`;
+//             alert(result);
+
+//             let playAgain = confirm('Play again?');
+//             playAgain ? location.reload() : alert('Ok, thanks for playing');
+//         }else{
+//             alert('you didnt enter rock, paper or scissor');
+//         }
+//     }else{
+//         alert('I guess you changed your mind. Maybe next time');
+//     }
+
+// }else{
+//     alert('Okay, maybe next time');
+// }
+
+
+
+
+
+
+//.................... MY PRACTICE....................//
+
+let playGame = confirm("Ready to play rock, paper or scissor?");
 
 if (playGame){
     //play
-    let playerChoice = prompt('please enter rock, paper or scissor.');
+    let playerChoice = prompt("Please enter either rock, paper or scissor");
 
-    if (playerChoice){
-        //continue to play
+    if(playerChoice){
+        //continue
         let playerOne = playerChoice.trim().toLowerCase();
-        if(playerOne === 'rock' || playerOne === 'paper' || playerOne === 'scissor'){
+        if (playerOne === 'rock' || playerOne === 'paper' || playerOne === 'scissor'){
 
             let computerChoice = Math.floor(Math.random() *3 +1);
-            let computer = computerChoice === 1 ? 'rock' : computerChoice === 2 ? 'paper' : 'scissor';
+            let computer = computerChoice === 1 ? 'rock' : computerChoice === 2? 'paper' : 'scissor' ;
 
-            let result = 
-            playerOne === computer ? `Tie game: \n${playerOne}\n${computer}`
-            : playerOne === 'rock' && computer === 'paper' ?
-            `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
-            : playerOne === 'paper' && computer === 'rock' ?
-            `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
-            : playerOne === 'scissor' && computer === 'rock' ?
-            `playerOne: ${playerOne}\ncomputer: ${computer}\ncomputer wins!`
-            : `playerOne: ${playerOne}\ncomputer: ${computer}\nplayerOne wins!`;
+            //result
+            let result =
+            playerOne === computer ? 'You tied the game'
+            :playerOne === 'paper' && computer === 'scissor' ? 
+            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins the game!`
+            :playerOne === 'paper' && computer === 'rock' ?
+            `PlayerOne: ${playerOne}\nComputer: ${computer}\nComputer wins the game!`
+            :`PlayerOne: ${playerOne}\nComputer: ${computer}\nPlayerOne wins the game!`;
+
             alert(result);
 
-            let playAgain = confirm('Play again?');
-            playAgain ? location.reload() : alert('Ok, thanks for playing');
+            let playAgain = confirm("Play Again?");
+            playAgain ? location.reload() : alert("Thanks for playing. See you next time.");
         }else{
-            alert('you didnt enter rock, paper or scissor');
+            alert("You didn't enter either rock, paper or scissor");
         }
     }else{
-        alert('I guess you changed your mind. Maybe next time');
+        alert("Oops! I guess you change your mind");
     }
-
 }else{
-    alert('Okay, maybe next time');
+    alert("Ok. Maybe next time");
 }
